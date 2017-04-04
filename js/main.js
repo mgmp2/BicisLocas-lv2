6a6162d810026600e6f121b8beea3139fdd4e5bb
Miriam Mendoza <miriammp1997@gmail.com>
2017-04-04 13:59:00 -0500

    extra

--- js/main.js
+++ js/main.js
@@ -2,27 +2,80 @@ document.getElementById("registrar").addEventListener("click", function (e){
   e.preventDefault();
   validateForm();
 })
-function validateForm(){
+var nom   = document.getElementById("name");
+var ap    = document.getElementById("lastname");
+var email = document.getElementById("input-email");
+var pas   = document.getElementById("input-password");
+var op    = document.getElementById("opciones");
 
-    var nom   = document.getElementById("name");
-    var ap    = document.getElementById("lastname");
-    var email = document.getElementById("input-email");
-    var pas   = document.getElementById("input-password");
-    var op    = document.getElementById("opciones");
+var expRegName  = /^([A-Z]{1}[a-z]+[\s]*)+$/g;
+var expRegLast  = /[A-Z]{1}([a-z]{2,12})+\s([A-Z]+([a-z]{2,10}))/g;
+var expRegEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+var expRegPass  = /^[a-zA-z]{6,}$/;
+var mens ="";
+var cont = 0;
 
-    var expRegName  = /^([A-Z]{1}[a-z]+[\s]*)+$/g;
-    var expRegLast  = /[A-Z]{1}([a-z]{2,12})+\s([A-Z]+([a-z]{2,10}))/g;
-    var expRegEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    var expRegPass  = /^[a-zA-z]{6,}$/;
-    var mens ="";
-    var cont = 0;
-
-    //Name
-    if(!expRegName.test(nom.value)|| nom.value ==""){
-      (!nom.value) ? mens = "Debe ingresar su nombre" : mens ="Debe empezar por Mayúscula";
-      createTooltip(nom.parentNode,mens);
+window.addEventListener("load", function (){
+  nom.addEventListener("blur",function(){
+    validationCorrect(nom,expRegName,  "Debe ingresar su nombre", "Debe empezar por Mayúscula");
+    });
+    ap.addEventListener("blur",function(){
+      validationCorrect(ap,expRegLast,  "Debe ingresar su apellido", "Debe contener sus dos apellidos empezando por Maýucula");
+      });
+  email.addEventListener("blur",function(){
+    validationCorrect(email,expRegEmail,  "Debe ingresar su email", "Verifique su e-mail");
+    });
+    pas.addEventListener("blur",function(){
+      validationCorrect(pas,expRegPass,  "Debe ingresar su contraseña", "La contraseña debe tener al menos seis carácteres");
+      });
+  op.addEventListener("blur",function(){
+    if(op.value=="0"){
+      mens="Debe seleccionar al menos un tipo de bici";
+      createTooltip(op.parentNode, mens);
       cont++;
     }
+  });
+
+
+  // validationCorrect(op,,  "Debe ingresar su nombre", "Debe empezar por Mayúscula");
+
+});
+
+function validationCorrect(inputL,expReg,mensaje1,mensaje2){
+          var padre = inputL.parentNode;
+          if(!expReg.test(inputL.value)|| inputL.value ==""){
+            var form = document.getElementById("box-form");
+            var hijos =form.children;
+            (!inputL.value) ? mens = mensaje1 : mens =mensaje2;
+            if(padre.lastElementChild.getAttribute("class") =="tooltiptext"){
+              console.log(clase);
+              var clase = padre.lastElementChild.getAttribute("class");
+              if(clase=="tooltiptext"){
+                padre.lastElementChild.classList.add("inactive");
+                padre.lastElementChild.innerHTML = mens;
+              }
+            } else {
+              var span = document.createElement("span");
+              span.setAttribute("class","tooltiptext");
+              span.innerHTML = mens;
+              padre.appendChild(span);
+            }
+          }
+          if(expReg.test(inputL.value)){
+            padre.lastChild.classList.add("inactive");
+          }
+      // hijos[i].lastChild.classList.add("inactive");
+}
+
+function validateForm(){
+    //Name
+    // if(!expRegName.test(nom.value)|| nom.value ==""){
+    //   (!nom.value) ? mens = "Debe ingresar su nombre" : mens ="Debe empezar por Mayúscula";
+      // onBlur(nom, mens);
+      // createTooltip(nom.parentNode,mens);
+
+      // cont++;
+    // }
 
     //Last Name
     if(!expRegLast.test(ap.value)){
@@ -68,14 +121,23 @@ function createTooltip(padre,mensaje){
     padre.appendChild(span);
   }
 }
-function validationCorrect(){
-  var form = document.getElementById("box-form");
-  var hijos =form.children;
-  for(var i=1; i<hijos.length; i++){
-    var clase = hijos[i].getAttribute("class");
-    var array = clase.split(" ");
-      if(array[1]=="input-box"){
-        hijos[i].lastChild.classList.add("inactive");
-      }
-  }
-}
+
+
+
+// function onBlur(hijo,mensaje){
+//
+//
+//   // createTooltip(nom.parentNode,mens);
+//   hijo.addEventListener("blur",function(){
+//   var padre = hijo.parentNode;
+//   if(padre.lastElementChild.getAttribute("class") =="tooltiptext"){
+//     padre.lastElementChild.innerHTML = mensaje;
+//   }else{
+//     var span = document.createElement("span");
+//     span.setAttribute("class","tooltiptext");
+//     span.innerHTML = mensaje;
+//     console.log(mensaje);
+//     padre.appendChild(span);
+//   }
+// });
+// }
