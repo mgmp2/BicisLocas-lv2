30a312ae14c96aa073d7f87d58052b815cf59502
Miriam Mendoza <miriammp1997@gmail.com>
2017-04-04 10:11:20 -0500

    show tooltip

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 document.getElementById("registrar").addEventListener("click", function (e){
   e.preventDefault();
   validateForm();
-
 })
 function validateForm(){
 
@@ -9,44 +8,48 @@ function validateForm(){
     var ap    = document.getElementById("lastname");
     var email = document.getElementById("input-email");
     var pas   = document.getElementById("input-password");
-    // var op    = document.getElementById("opciones");
+    var op    = document.getElementById("opciones");
 
     var expRegName  = /^([A-Z]{1}[a-z]+[\s]*)+$/g;
     var expRegLast  = /[A-Z]{1}([a-z]{2,12})+\s([A-Z]+([a-z]{2,10}))/g;
     var expRegEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     var expRegPass  = /^[a-zA-z]{6,}$/;
+    var mens ="";
     var cont = 0;
 
     //Name
     if(!expRegName.test(nom.value)|| nom.value ==""){
-      var mens ="Escribir correctamente \nCampo nombre: empezando por Mayúscula";
+      (!nom.value) ? mens = "Debe ingresar su nombre" : mens ="Debe empezar por Mayúscula";
       createTooltip(nom.parentNode,mens);
       cont++;
     }
 
     //Last Name
     if(!expRegLast.test(ap.value)){
-      var mens ="Escribir correctamente \nCampo apellido: Sus dos apellidos empezando por Maýucula";
+      (!ap.value) ? mens ="Debe ingresar su apellido" : mens="Debe contener sus dos apellidos empezando por Maýucula";
       createTooltip(ap.parentNode,mens);
       cont++;
     }
 
     //Email
     if(!expRegEmail.test(email.value)){
-          var mens = "Escribir correctamente \nCampo email de manera correcta";
+      (!email.value) ? mens="Debe ingresar su email" : mens = "Verifique su e-mail";
           createTooltip(email.parentNode, mens);
           cont++;
     }
 
     //Password
     if(!expRegPass.test(pas.value) || pas.value == "123456" || pas.value == "098754" || pas.value == ""){
-        var mens = "Escribir correctamente \nCampos de contraseña diferente de: 123456 o 98754";
+        mens = "La contraseña debe tener al menos seis carácteres";
         createTooltip(pas.parentNode, mens);
         cont++;
-    }
+    }if(op.value=="0"){
+        mens="Debe seleccionar al menos un tipo de bici";
+        createTooltip(op.parentNode, mens);
+        cont++;
 
-    else if(cont == 0){
-      alert("Muy bien \n Lleno los datos correctamente");
+    } if(cont == 0){
+        alert("Muy bien \n Lleno los datos correctamente");
 
     }
 }
@@ -54,8 +57,13 @@ function validateForm(){
 function createTooltip(padre,mensaje){
   // var padre   = document.getElementById("name").parentNode; se debe traer
 
-  var span = document.createElement("span");
-  span.innerHTML = mensaje;
-  span.setAttribute("class","tooltiptext");
-  padre.appendChild(span);
+
+  if(padre.lastChild.getAttribute("class") =="tooltiptext"){
+    alert("ya te cree");
+  }else{
+    var span = document.createElement("span");
+    span.innerHTML = mensaje;
+    span.setAttribute("class","tooltiptext");
+    padre.appendChild(span);
+  }
 }
